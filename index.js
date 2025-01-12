5a7de5768607c913674a4e0587bffba2096022a4
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-12 09:26:17 +0530

    added security

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const ping_pong = require('./src/ping-pong');
-app.use(ping_pong);
-
-
-
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const compression = require('compression');
+const connectToDB = require('./src/config/database');
+const ping_pong = require('./src/ping-pong');
+const api = require('./src/api');
+const signup = require('./src/router/signup');
+
+const app = express();
+
+app.use(helmet());
 app.use(
   cors({
     origin: process.env.FRONT_END_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
 );
-
-const corsOptions = {
-  origin: process.env.FRONT_END_URL,
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
-const api = require('./src/api');
-app.use(api);
-
-
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests, please try again later.',
+  })
+);
+app.use(compression());
 app.use(express.json());
-const signup = require('./src/router/signup');
+app.use(cookieParser());
+app.use(ping_pong);
+
+app.use((req, res, next) => {
+  if (req.headers.origin !== process.env.FRONT_END_URL) {
+    res.status(403).send('Access denied');
+  } else {
+    next();
+  }
+});
+
+app.use(api);
 app.use(signup);
-
-app.use((req, res) => res.send('Hello from server '));
-
-const connectToDB = require('./src/config/database');
-
-
-
+ 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Page not found' });
+});
+ 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
 
 connectToDB()
-.then(() => {
-  console.log('Connected to MongoDB');
+  .then(() => {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT} successfully`);
     });
   })
-  .catch((e) => {
-    console.log(e);
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
   });
