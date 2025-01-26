261372c0a1807c14254d7738b9564e9b53bae175
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-26 09:48:26 +0530

    changed cors

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');  
   }
 
   res.setHeader(
