e82b940439a483b8553ce200d92e783bde7d2765
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-28 11:40:11 +0530

    changed cors

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
+  'http://localhost:3001',
   'https://www.markethealers.com',
   'https://auth.markethealers.com',
   'https://server.markethealers.com',
