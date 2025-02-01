d3828f530b0767b0bdb7f2a69e3f379b8db28d19
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-02-01 13:40:56 +0530

    yes

--- index.js
+++ index.js
@@ -29,9 +29,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');  
   }
   next();
 });
+
 
  
 
