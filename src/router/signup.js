12dad119ac71fc7d8a18ab01e1e01786c7f2a2d1
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-02-08 14:15:05 +0530

    re done

--- src/router/signup.js
+++ src/router/signup.js
@@ -89,6 +89,7 @@ signup.get(
 );
 
 signup.get('/markethealers/auth/authCheck', async (req, res) => {
+  console.log(req.cookies); 
   try {
     const tokenByUser = req.cookies?.token;
     if (!tokenByUser) {
