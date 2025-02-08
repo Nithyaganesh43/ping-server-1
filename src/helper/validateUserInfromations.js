aa0d8c7d509447b42260b092b22ec27b78de1d74
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-02-08 14:24:08 +0530

    f

--- src/helper/validateUserInfromations.js
+++ src/helper/validateUserInfromations.js
@@ -4,16 +4,14 @@ async function validateUserInfromations(fullName, username, password, platform, 
     const user = await User.findOne({ platform, email });
     if (!user) throw new Error("User Not Found");
 
-    if (user.userName && user.fullName && user.password) throw new Error("User Already Registered");
-
-    if (username.length < 8 || username.length > 20) 
-        throw new Error("Username must be between 8 and 20 characters");
+    if (user.userName && user.fullName && user.password) throw new Error("User Already Registered You Can Login Now");
+ 
 
     const isUserNameUnique = await User.findOne({ userName: username });
     if (isUserNameUnique) throw new Error("Username already taken");
 
-    if (password.length < 8 || password.length > 20) 
-        throw new Error("Password must be between 8 and 20 characters");
+    if (password.length < 5 || password.length > 50) 
+        throw new Error("Password must be between 5 and 50 characters");
 
     if (fullName.length < 3 || fullName.length > 50) 
         throw new Error("Full name must be between 3 and 50 characters");
